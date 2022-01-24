8f094682890ebd21c761a712c9947c3b8ae06c92
youngsoo heo <3340513@naver.com>
2022-01-24 11:52:04 +0900

    s

---
+++ mysql/exe/exc.sql
@@ -0,0 +1,15 @@
+use world;
+
+show tables;
+
+	--테이블생성
+menber2create table menber2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetimecountrycountrymenbercountry
+    );
+    
+
+
+
