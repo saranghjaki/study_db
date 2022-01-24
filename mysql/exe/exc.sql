f226717d454d6fb226f9900396de1a6ebca1f6b7
youngsoo heo <3340513@naver.com>
2022-01-24 12:02:31 +0900

    a

--- mysql/exe/exc.sql
+++ mysql/exe/exc.sql
@@ -2,14 +2,23 @@ use world;
 
 show tables;
 
-	--테이블생성
-menber2create table menber2 (
+-- 테이블생성	
+create table member (
 	seq int,
     name varchar(100),
     id varchar(100),
-    regdate datetimecountrycountrymenbercountry
+    regdate datetime
     );
     
-
+create table member2menber (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+    );
+    
+    -- 테이블삭제
+    
+    
 
 
