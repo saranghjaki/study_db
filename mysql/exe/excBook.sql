301740978c1c570585155b2c614993445f8e50c9
youngsoo heo <3340513@naver.com>
2022-01-26 11:22:45 +0900

    d

--- mysql/exe/excBook.sql
+++ mysql/exe/excBook.sql
@@ -102,9 +102,89 @@ insert into books(
                
  );
  
- select * from spacerental;
+
+ select * from cafe;
  
+ CREATE TABLE IF NOT EXISTS `test` (
+  `seq` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`seq`))
+ENGINE = InnoDB;
  
+ insert test(
+ name
+ ) value(
+ " "
+ );
  
+ select* from test;
  
+CREATE TABLE IF NOT EXISTS `test2` (
+  `seq` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `seq2` INT NOT NULL,
+  `seq3` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`seq`, `seq3`, `seq2`));
   
+  insert test2(
+  seq
+   ,name
+  )value(
+  6
+  ,''
+  );
+  
+  select*from test2;
+  
+  -- 테이블수정
+  -- DDL : db, table: 관리자, 선임
+  -- DML : date; 후임들
+  
+  
+  -- 컬럼 추가
+  alter table test add tel varchar(50);
+  -- test table에 tel 이라는 varchar(50)추가
+  
+  -- 컬럼 삭제
+  alter table test drop tel;
+  
+  -- 컬럼 타입 변경
+  alter table test modify tel int;
+  -- alter table test modify tel varchar(20);
+  
+  -- 컬럼 타입 변경
+  alter table test change tel tele int;
+  
+  insert into test(
+	name
+    ,tele
+  )values (
+	'ys'
+    ,1111
+  );
+  
+  select*from test;
+  desc test;
+  
+  -- 데이터 수정
+  update test set
+	name='surang'
+    ,tele = '222'
+  where 1=1 
+ and  seq = 1;
+  
+  -- 데이터 삭제
+  delete from test
+  where 1=1
+	and seq = 3;
+    
+    -- 전체 데이터 삭제
+	truncate test;
+    
+    -- DML :date definition language :db table 컨트롤 : 주로 관리자, 선임들의 업무 alter
+    -- DDL : date manipulation language : data : 후임들
+    -- DCL : date control langyage : 접근권환 커밋.롤백
+    
+    -- insert, update, delete-> data 바로 영향을 미치는가?
+    
+    
